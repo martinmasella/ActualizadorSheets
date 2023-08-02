033e6d562213465dcac6d74f0e4fb3afc8366dc4
Martin Masella <pccito@yahoo.com>
2023-08-02 17:49:13 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ActualizadorSheets
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tmrRefresh = new System.Windows.Forms.Timer(this.components);
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(190, 12);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(76, 23);
+            this.txtClave.TabIndex = 18;
+            this.txtClave.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(148, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Clave";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(63, 12);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(76, 23);
+            this.txtUsuario.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Usuario";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(278, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(70, 24);
+            this.btnLogin.TabIndex = 13;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(354, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(70, 24);
+            this.btnClear.TabIndex = 19;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tmrRefresh
+            // 
+            this.tmrRefresh.Interval = 10000;
+            this.tmrRefresh.Tick += new System.EventHandler(this.tmrRefresh_Tick);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 15;
+            this.lstLog.Location = new System.Drawing.Point(8, 42);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(419, 94);
+            this.lstLog.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 143);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtClave;
+        private Label label5;
+        private TextBox txtUsuario;
+        private Label label6;
+        private Button btnLogin;
+        private Button btnClear;
+        private System.Windows.Forms.Timer tmrRefresh;
+        private ListBox lstLog;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Primary;
+using Primary.Data;
+
+namespace ActualizadorSheets
+{
+    public partial class Form1 : Form
+    {
+
+        const string sURL = "https://api.veta.xoms.com.ar";
+        const string prefijoPrimary = "MERV - XMEV - ";
+        const string sufijoCI = " - CI";
+        const string sufijo48 = " - 48hs";
+
+        string LEDEPesos;
+        string LEDEDolares;
+
+        string token;
+        List<string> nombres;
+        List<Ticker> tickers;
+
+        String spreadsheetId2 = "1W2kTK4n10-fKWYRJmoffOnxdaibkL-7I5wduSHVlGvI";
+
+        UserCredential credencial;
+        SheetsService service;
+        FileStream stream = new FileStream("client_secret_202947654746-fmqgbul2aegj1gqe66cjh5tb633oadcn.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read);
+        string credPath = "token.json";
+        static string[] Scopes = { "https://www.googleapis.com/auth/spreadsheets" };
+
+        String range2 = "Dato!B1";
+
+        public Form1()
+        {
+            InitializeComponent();
+            credencial = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            GoogleClientSecrets.FromStream(stream).Secrets,
+            Scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(credPath, true)).Result;
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credencial,
+                ApplicationName = "Arbitraje Ratios"
+            });
+            CheckForIllegalCrossThreadCalls = false;
+
+            var configuracion = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            LEDEPesos = configuracion.GetSection("MiConfiguracion:LEDE Pesos").Value;
+            LEDEDolares = configuracion.GetSection("MiConfiguracion:LEDE Dolares").Value;
+            txtUsuario.Text = configuracion.GetSection("MiConfiguracion:UsuarioVETA").Value;
+            txtClave.Text = configuracion.GetSection("MiConfiguracion:ClaveVETA").Value;
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // The A1 notation of the values to clear.
+            string range = "Intraday!O2:T2550";  // TODO: Update placeholder value.
+
+            // TODO: Assign values to desired properties of `requestBody`:
+            ClearValuesRequest requestBody = new ClearValuesRequest();
+            SpreadsheetsResource.ValuesResource.ClearRequest request = service.Spreadsheets.Values.Clear(requestBody, spreadsheetId2, range);
+
+            // To execute asynchronously in an async method, replace `request.Execute()` as shown:
+            ClearValuesResponse response = request.Execute();
+            // Data.ClearValuesResponse response = await request.ExecuteAsync();
+
+            // TODO: Change code below to process the `response` object:
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+            ToLog("Cleared");
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Inicio();
+        }
+
+        private async Task Inicio()
+        {
+            var api = new Api(new Uri(sURL));
+            await api.Login(txtUsuario.Text, txtClave.Text);
+            ToLog("Login VETA Ok");
+
+            var allInstruments = await api.GetAllInstruments();
+
+            var entries = new[] { Entry.Last, Entry.Bids, Entry.Offers };
+
+            FillListaTickers();
+
+            var instrumentos = allInstruments.Where(c => tickers.Any(t => t.NombreLargo == c.Symbol));
+            using var socket = api.CreateMarketDataSocket(instrumentos, entries, 1, 1);
+            socket.OnData = OnMarketData;
+            var socketTask = await socket.Start();
+            socketTask.Wait(1000);
+            ToLog("Websocket Ok");
+            tmrRefresh.Interval = 10000;
+            tmrRefresh.Enabled = true;
+            tmrRefresh.Start();
+
+            await socketTask;
+        }
+
+        private async void OnMarketData(Api api, MarketData marketData)
+        {
+            try
+            {
+                var ticker = marketData.Instrument.Symbol;
+                //var ticker = marketData.InstrumentId.Symbol;
+                decimal bid = 0;
+                if (marketData.Data.Bids != null)
+                {
+                    bid = marketData.Data.Bids.FirstOrDefault().Price;
+                }
+                decimal offer = 0;
+                if (marketData.Data.Offers != null)
+                {
+                    offer = marketData.Data.Offers.FirstOrDefault().Price;
+                }
+                decimal last = 0;
+                if (marketData.Data.Last != null)
+                {
+                    last = marketData.Data.Last.Price;
+                }
+
+                var elemento = tickers.Where<Ticker>(t => t.NombreLargo == ticker).FirstOrDefault<Ticker>();
+                elemento.bid = bid;
+                elemento.offer = offer;
+                elemento.last = last;
+
+                ToLog(ticker);
+            }
+            catch (Exception ex)
+            {
+                ToLog(ex.Message);
+            }
+        }
+
+        private void FillListaTickers()
+        {
+            nombres.Add("GD30");
+            nombres.Add("AL30");
+            nombres.Add("GD30D");
+            nombres.Add("AL30D");
+            nombres.Add(LEDEPesos);
+            nombres.Add(LEDEDolares);
+
+            foreach (var nombre in nombres)
+            {
+                tickers.Add(new Ticker(prefijoPrimary + nombre + sufijoCI, nombre + "CI", nombre));
+                tickers.Add(new Ticker(prefijoPrimary + nombre + sufijo48, nombre + "48", nombre));
+            }
+        }
+
+        private void ToLog(string s)
+        {
+            lstLog.Items.Add(DateTime.Now.ToLongTimeString() + ": " + s);
+            lstLog.SelectedIndex = lstLog.Items.Count - 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Top = 10;
+            this.Text = "Actualizador Sheets";
+            nombres = new List<string>();
+            tickers = new List<Ticker>();
+        }
+
+        private void tmrRefresh_Tick(object sender, EventArgs e)
+        {
+            ToLog("Tick");
+            string ahora = DateTime.Now.ToString("HH:mm:ss");
+
+            decimal PGD3048 = Precio("GD3048", "Precio");
+            decimal PAL3048 = Precio("AL3048", "Precio");
+            decimal ratio = 0;
+            if (PGD3048 > 0 && PAL3048 > 0)
+            {
+                ratio = Math.Round(((PGD3048 / PAL3048) - 1) * 100, 2);
+            }
+            decimal PGD30V = Precio("GD30CI", "Venta");
+            decimal PGD30DC = Precio("GD30DCI", "Compra");
+            decimal PAL30DV = Precio("AL30DCI", "Venta");
+            decimal PAL30C = Precio("AL30CI", "Compra");
+            decimal PAL30V = Precio("AL30CI", "Venta");
+            decimal PAL30DC = Precio("AL30DCI", "Compra");
+            decimal PGD30DV = Precio("GD30DCI", "Venta");
+            decimal PGD30C = Precio("GD30CI", "Compra");
+            decimal PGD30V48 = Precio("GD3048", "Venta");
+            decimal PGD30C48 = Precio("GD3048", "Compra");
+            decimal PAL30V48 = Precio("AL3048", "Venta");
+            decimal PAL30C48 = Precio("AL3048", "Compra");
+            decimal PLEDEV = Precio(LEDEPesos + "CI", "Venta");
+            decimal PLEDEC = Precio(LEDEPesos + "CI", "Compra");
+            decimal PLEDEDV = Precio(LEDEDolares + "CI", "Venta");
+            decimal PLEDEDC = Precio(LEDEDolares + "CI", "Compra");
+            decimal PGD30D = Precio("GD30D48", "Precio");
+            decimal PAL30D = Precio("AL30D48", "Precio");
+            decimal PAL30D48V = Precio("AL30D48", "Venta");
+            decimal PAL30D48C = Precio("AL30D48", "Compra");
+
+            decimal PuntasGDAL = 0;
+            decimal PuntasALGD = 0;
+            if (PGD30C48 > 0 && PAL30V48 > 0 && PGD30V48 > 0 && PAL30C48 > 0)
+            {
+                PuntasGDAL = Math.Round(((PGD30C48 / PAL30V48) - 1) * 100, 2);
+                PuntasALGD = Math.Round(((PGD30V48 / PAL30C48) - 1) * 100, 2);
+            }
+
+            if (int.Parse(DateTime.Now.ToString("HHmm")) >= 1103 && int.Parse(DateTime.Now.ToString("HHmm")) < 1702)
+            {
+                ValueRange valueRange = new ValueRange();
+                valueRange.MajorDimension = "COLUMNS";//"ROWS";//COLUMNS
+
+                /*
+                var oblist = new List<object>() { PBono1, PBono2, ratio,
+                    PGD30V, PGD30DC, PAL30DV, PAL30C, PAL30V, PAL30DC, PGD30DV, PGD30C,
+                    PGD30V48, PGD30C48, PAL30V48, PAL30C48, ledeV,ledeC, ledeDV,ledeDC,GD30D,AL30D};
+                */
+                var oblist = new List<object>() { PGD3048, PAL3048, ratio,
+                    PGD30V, PGD30DC, PAL30DV, PAL30C, PAL30V, PAL30DC, PGD30DV, PGD30C,
+                    PGD30V48, PGD30C48, PAL30V48, PAL30C48, PLEDEV, PLEDEC, PLEDEDV,
+                    PLEDEDC, PGD30D, PAL30D,PAL30D48V,PAL30D48C};
+
+                valueRange.Values = new List<IList<object>> { oblist };
+
+                SpreadsheetsResource.ValuesResource.UpdateRequest update;
+                update = service.Spreadsheets.Values.Update(valueRange, spreadsheetId2, range2);
+                update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+                UpdateValuesResponse result2 = update.Execute();
+
+
+                //Intraday
+
+                ValueRange lectura = service.Spreadsheets.Values.Get(spreadsheetId2, "Intraday!A1:A2550").Execute();
+
+                int siguiente = lectura.Values.Count + 1;
+                valueRange = new ValueRange();
+                valueRange.MajorDimension = "ROWS";
+                oblist = new List<object>() { ahora, PGD3048, PAL3048, ratio, PuntasGDAL, PuntasALGD };
+                valueRange.Values = new List<IList<object>> { oblist };
+                update = service.Spreadsheets.Values.Update(valueRange, spreadsheetId2, "Intraday!O" + siguiente.ToString() + ":T" + siguiente.ToString());
+                update.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+                try
+                {
+                    update.Execute();
+
+                }
+                catch (Exception ex)
+                {
+                    ToLog(ex.Message);
+                }
+
+            }
+
+        }
+
+        private decimal Precio(string nombremedio, string cual)
+        {
+            decimal precio = 0;
+            var ticker = tickers.Where<Ticker>(t => t.NombreMedio == nombremedio).FirstOrDefault();
+            switch (cual)
+            {
+                case "Precio":
+                    precio = ticker.last;
+                    break;
+                case "Venta":
+                    precio = ticker.offer;
+                    break;
+                case "Compra":
+                    precio = ticker.bid;
+                    break;
+            }
+            return precio;
+        }
+
+
+
+    }
+    public class Ticker
+    {
+        public string NombreLargo { get; set; }
+        public string NombreMedio { get; set; }
+        public string NombreCorto { get; set; }
+        public decimal bid;
+        public decimal last;
+        public decimal offer;
+        public Ticker(string nombrelargo, string nombremedio, string nombrecorto)
+        {
+            NombreLargo = nombrelargo;
+            NombreMedio = nombremedio;
+            NombreCorto = nombrecorto;
+            bid = 0;
+            last = 0;
+            offer = 0;
+        }
+    }
+}
